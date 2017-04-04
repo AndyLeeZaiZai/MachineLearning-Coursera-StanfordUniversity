696a30245f0209247a9055f4e54b0701ed95bb8d
AndyLeeZaiZai <494447856@qq.com>
2017-04-04 10:59:55 +0200

    little revise

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,8 +16,6 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    % Nested loop implementation:
-	% 
     temp = (1/m)*X'*(X*theta - y);
     theta = theta - (alpha*temp);
     
